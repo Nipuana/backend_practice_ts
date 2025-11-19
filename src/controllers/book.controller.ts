ccd9b168f63d7a00628f90b3209209d19f65ebab
Nipuana <nipuanarimal@gmail.com>
2025-11-19 08:50:17 +0545

    added dto autovalidation

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -1,9 +1,22 @@
 import {Request,Response} from "express"
-export type Book={
-    id:string,
-    title:string,
-    date?:string
-}
+import {z} from 'zod'
+
+export const BookSchema=z.object({
+    id:z.string().min(1,{message:"ID is required"}),
+    title:z.string().min(1,{message:"Title is required"}),
+    date:z.string().optional()
+})
+
+export type Book = z.infer<typeof BookSchema>
+
+// export type Book={
+//     id:string,title:string,date?:string
+// }
+
+//DTO-Data Transfer Object
+//Inpu/Output Structure
+export const createBookDTO=BookSchema.pick({id:true,title:true})
+export type createBookDTO= z.infer<typeof createBookDTO>
 
 let  books:Book[]=[
     {id:'0-1',title:"Aauish"},
@@ -12,13 +25,19 @@ let  books:Book[]=[
 ]
 export class BookController {
     createBook(req:Request,res:Response){
+
+        const parsedBook=createBookDTO.safeParse(req.body)
+        // Auto Validation
+        if(!parsedBook.success){
+            return res.status(400).json({errors: parsedBook.error})
+        }
         const {id,title}= req.body //destructure
-        if(!title){
-            return res.status(400).json({message:"Title is required"})
-        }
-        if(!id){
-            return res.status(400).json({message:"ID is required"})
-        }
+        // if(!title){
+        //     return res.status(400).json({message:"Title is required"})
+        // }
+        // if(!id){
+        //     return res.status(400).json({message:"ID is required"})
+        // }
         const checkId: boolean = books.some((book)=>book.id===id)
         if(checkId){
             return res.status(409).json({message:"Book with this ID already exists"})
