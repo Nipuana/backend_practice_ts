280bc97965e0556ccc4f2d196609d88e649f6ffd
Nipuana <nipuanarimal@gmail.com>
2025-11-25 13:29:08 +0545

    added all files of user and done user controller , router and oindex with user loigc

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express,{Application,Request,Response} from 'express';
 import bookRoutes from './routes/book.route'
+import userRoutes from './routes/user.route'
 
 const app:Application = express();
 const PORT:number = 3000;
@@ -7,10 +8,8 @@ const PORT:number = 3000;
 app.get('/',(req:Request,res:Response)=>{
     res.send('Hello world');
 });
-
-
-
 app.use('/api/books',bookRoutes)
+app.use('/api/users',userRoutes)
 
 app.listen(PORT,()=>{
     console.log(`Server is running at http://localhost:${PORT}`);
---
+++ src/routes/user.route.ts
@@ -0,0 +1,24 @@
+import {Router,Request,Response} from "express"
+import {UserController} from "../controllers/user.controller";
+
+export const router = Router();
+const userController = new UserController();
+
+
+// POST new user - 
+router.post('/c',userController.createBook );
+
+router.get('/g',userController.getUsers );
+
+// GET single user - 
+router.get('/g/:id', userController.getUserById );
+
+
+
+// PUT update user - 
+router.put('/api/users/:id',userController.putUser);
+
+// DELETE user - 
+router.delete('/api/users/:id',userController.deleteUser);
+
+export default router;
